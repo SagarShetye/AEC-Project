b8802895e170562a991801f23fad01be834235bf
HarshGehani <33762936+HarshGehani@users.noreply.github.com>
2018-04-23 00:24:46 -0400

    Update meanValueVisualizer.h

--- Mean_Value_Coordinates/MeanValueVisualizer/meanValueVisualizer.h
+++ Mean_Value_Coordinates/MeanValueVisualizer/meanValueVisualizer.h
@@ -12,6 +12,8 @@
 #include "ysclass.h"
 #include "fslazywindow.h"
 
+#include <ysshellext.h>
+
 #include "objloader.h"
 
 // Class for mesh visualization
@@ -27,16 +29,48 @@ private:
 	std::vector <float> vtx, nom, col,
 		vtx_control, nom_control, col_control;
 
+	// vector for storing picked vertices for deformation:
+	std::vector <PolygonalMesh::VertexHandle> vertexVec;
+
+	// function stores vextex Handle selected by user:
+	void storeVertex(PolygonalMesh::VertexHandle vtHd);
+
+	// vector for storing the mouse positions for deformation:
+	std::vector < std::vector <int>> mouseVec;
+
+	// function deforms model as per mouse input:
+	void Deform(std::vector < std::vector <int>> positions);
+
+
+
 public:
 	meanValueVis();
 	void meanValueInterpolateDeformation();
 	bool Initialize(const char model_fn[], const char control_fn[]);
 	void colourAllPolygons(PolygonalMesh mesh, YsColor colour);										// Adds colour to all the polygons in the mesh
 	void draw() const;
-	void RemakeVertexArray();
+	//void RemakeVertexArray();
 	static void AddColor(std::vector <float> &col, float r, float g, float b, float a);
 	static void AddVertex(std::vector <float> &vtx, float x, float y, float z);
 	static void AddNormal(std::vector <float> &nom, float x, float y, float z);
+
+
+	void RemakeVertexArray(void);
+	YsMatrix4x4 GetProjection(void) const;
+	YsMatrix4x4 GetModelView(void) const;
+	PolygonalMesh::PolygonHandle PickedPlHd(int mx, int my) const;
+	PolygonalMesh::VertexHandle PickedVtHd(int mx, int my, int pixRange) const;
+
+	// function to initialize vertices for mesh deformation: 
+	void DeformSelection();
+
+	// function stores mouse positions:
+	void mousePositions(void);
+
+	// function clears all selected vertices:
+	void Clear();
+
+
 	void move();
 	void deformControlMesh();
 	bool terminate();
